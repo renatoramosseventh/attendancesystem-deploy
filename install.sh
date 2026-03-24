5606184c2e2b531edd0fc2c9d7248b6972cc5a8d
renator2 <renato.ramos@seventh.com.br>
2026-03-24 20:14:42 -0300

    #main - definição de builds e tags

--- install.sh
+++ install.sh
@@ -387,7 +387,6 @@ ASPNETCORE_ENVIRONMENT=Production
 DOCKER_REPO=seventhltda
 IMAGEM_LATEST=latest
 IMAGEM_UNSTABLE_LATEST=unstable-latest
-IMAGEM_V1=v1
 NGINX_DEFAULT_PORT=80
 
 # ── MongoDB ───────────────────────────────────────────────
